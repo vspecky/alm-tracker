6e2430ca270427e5713adc2bc2b0ca7e20d7dee2
vspecky <thelonenerd9913@gmail.com>
2020-01-18 12:30:39 +0530

    Added chat feature

--- src/Public/chatRoom.js
+++ src/Public/chatRoom.js
@@ -1,5 +1,65 @@
 const socket = io("http://localhost:5000");
+const chatContainer = document.getElementById("alm-chat-container");
+const msgForm = document.getElementById("msg-form");
+const msgInput = document.forms["msg-form"]["msg-input"];
 
-socket.on("chat-message", data => {
-    console.log(data);
-})
+myUsr = JSON.parse(myUsr);
+
+console.log(myRoom);
+
+socket.emit("new-user", myRoom)
+
+socket.on("chat-message", (user, msg) => {
+    createMessage(user, msg);
+});
+
+msgForm.addEventListener("submit", e => {
+    e.preventDefault();
+    const msg = msgInput.value;
+    socket.emit("send-chat-message", myUsr, msg, myRoom);
+    createMessage(myUsr, msg);
+    msgInput.value = "";
+});
+
+const createMessage = (myuser, msg) => {
+    const outer = document.createElement("div");
+    outer.classList.add("card");
+    outer.classList.add("border-0");
+    //outer.classList.add("mt-1");
+    //outer.classList.add("ml-1");
+    //outer.classList.add("mr-1");
+    //outer.classList.add("mw-100");
+    chatContainer.append(outer);
+
+    console.log(myuser);
+
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+    outer.append(cardBody);
+
+    const media = document.createElement("div");
+    media.classList.add("media");
+    cardBody.append(media);
+
+    const profImg = document.createElement("img");
+    profImg.classList.add("align-self-start");
+    profImg.classList.add("mr-3");
+    profImg.classList.add("rounded-circle");
+    profImg.classList.add("prof-img");
+    profImg.src = `/images/${myuser.id}`;
+    media.append(profImg);
+
+    const mediaBody = document.createElement("div");
+    mediaBody.classList.add("media-body");
+    media.append(mediaBody);
+
+    const name = document.createElement("a");
+    name.href = `/users/dashboard/${myuser.id}`;
+    name.innerHTML = myuser.name;
+    mediaBody.append(name);
+
+    const content = document.createElement("p");
+    content.classList.add("prevent-overflow");
+    content.innerHTML = msg;
+    mediaBody.append(content);
+}
--- src/Routers/chats.js
+++ src/Routers/chats.js
@@ -1,10 +1,42 @@
 const router = module.exports = require("express").Router();
 const io = require("socket.io")(5000);
+const authCheckers = require("../Utils/authCheckers.js");
+const rooms = ["comp", "mech", "civil", "it", "etx"];
 
 io.on("connection", socket => {
-    socket.emit("chat-message", "Hello World!");
+    socket.on("new-user", room => {
+        socket.join(room);
+    })
+    socket.on("send-chat-message", (user, msg, room) => {
+        socket.to(room).broadcast.emit("chat-message", user, msg);
+    });
 });
 
-router.get("/", (req, res) => {
-    res.render("chat_room");
+router.get("/", authCheckers.checkAuth, (req, res) => {
+    res.render("chats", {
+        auth: req.isAuthenticated(),
+        admin: req.user.admin,
+        reqUser: req.user
+    });
+})
+
+router.get("/:room", authCheckers.checkAuth, (req, res) => {
+
+    if (!rooms.includes(req.params.room)) {
+        req.flash("danger", "Invalid room.");
+        res.redirect("/chats");
+    }
+
+    const usrDetails = {
+        id: req.user._id,
+        name: `${req.user.firstName} ${req.user.lastName}`
+    };
+
+    res.render("chat_room", {
+        userDetails: JSON.stringify(usrDetails),
+        myRoom: `${req.params.room}`,
+        auth: req.isAuthenticated(),
+        admin: req.user.admin,
+        reqUser: req.user
+    });
 });
